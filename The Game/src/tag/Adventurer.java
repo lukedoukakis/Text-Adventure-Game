b9eb6a33fb93ea0e7d32cc1baf6b49f70ba9b09b
Nicholas Baron <njbaron@cpp.edu>
2018-01-18 12:43:47 -0800

    Start of Game

---
+++ The Game/src/tag/Adventurer.java
@@ -0,0 +1,23 @@
+package tag;
+
+public class Adventurer {
+
+	private int gold, lvl;
+	
+	private char type;
+	
+	private String name;
+	
+	public Adventurer(final String name, final char type) {
+		
+	}
+
+	public void attack(final char enemy, final int moveSelection) {
+		
+	}
+	
+	public void sneak() {//Is this method really needed?
+		
+	}
+	
+}
---
+++ The Game/src/tag/Main.java
@@ -0,0 +1,14 @@
+package tag;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static final Scanner input = new Scanner(System.in);
+	
+	public static void main(final String[] args) {
+		
+
+	}
+
+}
